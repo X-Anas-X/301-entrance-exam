88c01fd57515d161f57f6f0329a2c41d095ba5e2
X-Anas-X <anas.zain1996@gmail.com>
2020-03-16 14:58:57 +0200

    not done yet

---
+++ js/q3.js
@@ -0,0 +1,32 @@
+'use strict';
+var coursesArr = [];
+
+
+function Course(name){
+  this.name = name;
+  coursesArr.push(this);
+  setItem();
+
+}
+function setItem(){
+  var pickedCourse = JSON.stringify(coursesArr);
+  localStorage.setItem('course Name', pickedCourse);
+}
+
+function getItem(){
+  var myCourse = localStorage.getItem('myCourse');
+  coursesArr = JSON.parse(myCourse);
+
+}
+for(var j=0; j< coursesArr.length; j++){
+  new Course(coursesArr[j]);
+}
+
+
+var form = document.getElementById('forms');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var firstLine = event.target.course.value;
+  var dateLine = event.target.start.value;
+});
+getItem();
